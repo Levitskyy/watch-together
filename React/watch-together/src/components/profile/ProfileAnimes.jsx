bad5dbe6465b7e17ce691a1e6174886216bd4e64
Levitskyy <Levitskyy@users.noreply.github.com>
2024-10-25 19:23:50 +0300

    made v1 of marked animes menu

--- React/watch-together/src/components/profile/ProfileAnimes.jsx
+++ React/watch-together/src/components/profile/ProfileAnimes.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
-import { capitalize } from "../../utils/utils";
+import { capitalize, formatDate } from "../../utils/utils";
 import axiosInstance from "../axiosInstance";
 import { serverURL } from "../../App";
+import { Link } from "react-router-dom";
 
 const CategoryButton = ({ clickHandler, text, isActive }) => {
     return (
@@ -14,6 +15,31 @@ const CategoryButton = ({ clickHandler, text, isActive }) => {
     );
 };
 
+const ProfileAnimeCard = ({ markedAnime }) => {
+    return (
+        <Link to={`/anime/${markedAnime.anime.id}`} className="p-1 hover:bg-neutral-700 rounded">
+            <div className="flex">
+                <div className="w-16 mr-2 my-1 relative">
+                    <img src={markedAnime.anime.poster_url} className="h-24 w-full rounded-lg"></img>
+                </div>
+                <div className="flex w-full justify-between">
+                    <div className="flex-col">
+                        <h2 className="text-neutral-200 text-lg">
+                            {markedAnime.anime.title}
+                        </h2>
+                        <span className="text-neutral-400 text-base">
+                            Добавлено {formatDate(markedAnime.updated_at)}
+                        </span>
+                    </div>
+                    <div className="text-neutral-400 text-xl">
+                        {markedAnime.rating ? markedAnime.rating : 'Не оценено'} ⭐
+                    </div>
+                </div>
+            </div>
+        </Link>
+    );
+}; 
+
 const statusList = [
     { ru: 'оценено', en: 'rated' },
     { ru: 'смотрю', en: 'watching' },
@@ -25,9 +51,14 @@ const statusList = [
     { ru: 'отложено', en: 'on_hold' }
 ];
 
+function getEnglishStatus(ruStatus) {
+    const status = statusList.find(item => item.ru === ruStatus);
+    return status ? status.en : null;
+}
+
 const ProfileAnimes = () => {
     const [currentCategory, setCurrentCategory] = useState('rated');
-    const [animeDict, setAnimeDict] = useState(null);
+    const [animeDict, setAnimeDict] = useState([]);
     // {
     //     anime: '',
     //     category: '',
@@ -62,7 +93,7 @@ const ProfileAnimes = () => {
 
     return (
         <div className="flex gap-2">
-            <div className="flex flex-col w-1/6 p-2 rounded bg-neutral-800">
+            <div className="flex flex-col w-1/6 p-2 rounded bg-neutral-800 h-fit">
                 {statusList.map((category) => (
                     <CategoryButton
                         text={capitalize(category.ru)}
@@ -72,7 +103,21 @@ const ProfileAnimes = () => {
                 ))}
             </div>
             <div className="flex flex-col w-5/6 p-2 rounded bg-neutral-800">
-                
+                {animeDict
+                    .filter((anime) =>
+                        currentCategory === 'rated' ? anime.rating !== null : getEnglishStatus(anime.category) === currentCategory
+                    )
+                    .map((anime) => (
+                        <ProfileAnimeCard key={anime.id} markedAnime={anime} />
+                    ))}
+                {animeDict
+                    .filter((anime) =>
+                        currentCategory === 'rated' ? anime.rating !== null : getEnglishStatus(anime.category) === currentCategory
+                    ).length === 0 && (
+                    <div className="h-full w-full flex items-center justify-center text-neutral-400">
+                        Добавьте тайтлы в категорию
+                    </div>
+                )}
             </div>
         </div>
     );
