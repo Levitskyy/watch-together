e457897963ccd9e5dd9c1e86a792119c3b922f92
Levitskyy <Levitskyy@users.noreply.github.com>
2024-10-27 02:49:04 +0300

    marked animes front end sort first version

--- React/watch-together/src/components/profile/ProfileAnimes.jsx
+++ React/watch-together/src/components/profile/ProfileAnimes.jsx
@@ -51,6 +51,17 @@ const statusList = [
     { ru: 'отложено', en: 'on_hold' }
 ];
 
+const sortList = [
+    { ru: 'По рейтингу', en: 'rating' },
+    { ru: 'По дате добавления', en: 'date' },
+    { ru: 'По названию', en: 'name' },
+];
+
+const ascList = [
+    { ru: 'По возрастанию', en: 'asc' },
+    { ru: 'По убыванию', en: 'desc' },
+];
+
 function getEnglishStatus(ruStatus) {
     const status = statusList.find(item => item.ru === ruStatus);
     return status ? status.en : null;
@@ -58,6 +69,8 @@ function getEnglishStatus(ruStatus) {
 
 const ProfileAnimes = () => {
     const [currentCategory, setCurrentCategory] = useState('rated');
+    const [sortAsc, setSortAsc] = useState('desc');
+    const [currentSort, setCurrentSort] = useState('rating');
     const [animeDict, setAnimeDict] = useState([]);
     // {
     //     anime: '',
@@ -91,22 +104,77 @@ const ProfileAnimes = () => {
         setCurrentCategory(category);
     };
 
+    const handleSortClick = (sort) => {
+        setCurrentSort(sort);
+    };
+
+    const handleAscClick = (asc) => {
+        setSortAsc(asc);
+    };
+
+    const getSort = (sortName, asc) => {
+        const sort = (a, b) => {
+            if (sortName === 'rating') {
+                const c = (a.rating - b.rating);
+                console.log("rating + " + c);
+                return (asc === 'asc' ? c : -c);
+            }
+            if (sortName === 'name') {
+                const c = (a.anime.title - b.anime.title);
+                console.log("name + " + c);
+                return (asc === 'asc' ? c : -c);
+            }
+            if (sortName === 'date') {
+                const c = (a.updated_at - b.updated_at);
+                console.log("date + " + c);
+                return (asc === 'asc' ? c : -c);
+            }
+        };
+
+        return sort;
+    };
+
     return (
         <div className="flex gap-2">
             <div className="flex flex-col w-1/6 p-2 rounded bg-neutral-800 h-fit">
-                {statusList.map((category) => (
-                    <CategoryButton
-                        text={capitalize(category.ru)}
-                        clickHandler={() => handleCategoryClick(category.en)}
-                        isActive={currentCategory === category.en}
-                    />
-                ))}
+                <div className="flex flex-col w-full">
+                    {statusList.map((category) => (
+                        <CategoryButton
+                            text={capitalize(category.ru)}
+                            clickHandler={() => handleCategoryClick(category.en)}
+                            isActive={currentCategory === category.en}
+                        />
+                    ))}
+                </div>
+                <div className="flex my-2">
+                    <h2 className="text-neutral-300">Сортировка</h2>
+                </div>
+                <div className="flex flex-col w-full">
+                    {sortList.map((sort) => (
+                        <CategoryButton
+                            text={capitalize(sort.ru)}
+                            clickHandler={() => handleSortClick(sort.en)}
+                            isActive={currentSort === sort.en}
+                        />
+                    ))}
+                </div>
+                <div className="border-b border-neutral-400 w-full my-2"/>
+                <div className="flex flex-col w-full">
+                    {ascList.map((asc) => (
+                        <CategoryButton
+                            text={capitalize(asc.ru)}
+                            clickHandler={() => handleAscClick(asc.en)}
+                            isActive={sortAsc === asc.en}
+                        />
+                    ))}
+                </div>
             </div>
             <div className="flex flex-col w-5/6 p-2 rounded bg-neutral-800">
                 {animeDict
                     .filter((anime) =>
                         currentCategory === 'rated' ? anime.rating !== null : getEnglishStatus(anime.category) === currentCategory
                     )
+                    .sort(getSort(currentSort, sortAsc))
                     .map((anime) => (
                         <ProfileAnimeCard key={anime.id} markedAnime={anime} />
                     ))}
