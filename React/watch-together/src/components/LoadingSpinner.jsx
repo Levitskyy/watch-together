6540e3a7d3e0298d76c32116ed0b8b255ba8f1f4
Levitskyy <Levitskyy@users.noreply.github.com>
2024-10-30 01:47:36 +0300

    removed react app

--- React/watch-together/src/components/LoadingSpinner.jsx
+++
@@ -1,7 +0,0 @@
-const LoadingSpinner = () => (
-    <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-neutral-200"></div>
-    </div>
-  );
-
-export default LoadingSpinner;
